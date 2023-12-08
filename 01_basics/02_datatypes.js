4fe12bfedbd5b1ebafda73e7adb4d439b6885e0d
Sagar Thotia <sthotia84@gmail.com>
2023-12-08 15:55:01 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3); // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code reusability should be high
+
+console.log("Sagar");
+
+let name = "Sagar";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint => // It will use in StockMarket, Trading, Reddit, Facebook.
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => Symbol is unique mostly use to identifying unique components in react; // Figma tools 
+
+
+// Object 
+
+console.log(typeof "Sagar"); // string
+console.log(typeof age); // number  
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
