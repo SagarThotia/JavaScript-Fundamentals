0134df9781ec9ffde6d4c22b93e7931c37124b9f
Sagar Thotia <sthotia84@gmail.com>
2023-12-08 17:15:31 +0000

    Comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,6 +8,8 @@ console.log(2 != 1);
 console.log("2" > 1);
 console.log("02" > 1);
 
+// Mostly avoid this type of code
+/*
 console.log();
 console.log(null > 0);
 console.log(null == 0);
@@ -17,6 +19,7 @@ console.log();
 console.log(undefined > 0);
 console.log(undefined == 0); 
 console.log(undefined >= 0);
+*/
 
 console.log(2 == 3); // It check only values
 
