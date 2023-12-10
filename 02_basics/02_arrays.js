c9e392d3b1ca813da53ba3c2f30479cb04d69d38
Sagar Thotia <sthotia84@gmail.com>
2023-12-10 02:20:34 +0000

    Arrays part2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,18 +1,18 @@
 const marvel_heros = ["Thor", "Ironman", "Spiderman"];
 const dc_heros = ["Batman","Flash","Superman"];
 
-// marvel_heros.push(dc_heros);
+marvel_heros.push(dc_heros);
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]); // This is not a better way merge arrays
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]); // This is not a better way merge arrays
 
-// let allHeros = marvel_heros.concat(dc_heros);
-// console.log(allHeros);
+let allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
 
 // more efficient method
 // To spread things or If you drop a glass it will fall and shattered and spread every where.
 const all_new_heros = [...marvel_heros, ...dc_heros]; 
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
