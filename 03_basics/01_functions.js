b2655bd117ee1b4bb6f103fc0cb813ed7d5da710
Sagar Thotia <sthotia84@gmail.com>
2023-12-11 14:42:21 +0000

    Functions Part1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,67 @@
+
+function sayMyName()
+{
+    console.log("S");
+    console.log("A");
+    console.log("G");
+    console.log("A");
+    console.log("R");
+}
+
+// sayMyName();
+
+// Way:1
+                        // (Parameters, arguments)
+// function  addTwoNumbers(number1, number2)
+// {
+//     console.log(number1 + number2);
+// }
+
+// Way:2
+// function  addTwoNumbers(number1, number2)
+// {
+    //     return number1 + number2;
+    // }
+    
+// Way:3
+function addTwoNumbers(number1, number2)
+{
+    let result = number1 + number2;
+    return result;
+}
+
+addTwoNumbers(5,2); // 7
+addTwoNumbers(5,"2"); // 52
+addTwoNumbers(5,"a"); // 5a
+addTwoNumbers(5, null); // 5
+        // (Parameters, arguments)
+
+
+// most interesting doubt and interview quesions for beginner        
+const result = addTwoNumbers(3, 5);
+console.log(`Result: ${result}`);
+
+
+                          // bydefault parameter value assigned is : sam  
+function loginUserMessage(username = "sam")
+{
+    // if(!username === undefined){
+    if(!username)
+    {
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("Sagar")); // Sagar just logged in
+console.log(loginUserMessage()); // undefined just logged in
+
+let logged =  loginUserMessage("Sagar"); 
+console.log(logged); // Sagar just logged in
+
+
+
+
+
+
