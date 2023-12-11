801dfb2a998cc30b1ba3f879ae0a9ff5beee3e86
Sagar Thotia <sthotia84@gmail.com>
2023-12-11 15:15:51 +0000

    Functions Part2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -61,7 +61,44 @@ let logged =  loginUserMessage("Sagar");
 console.log(logged); // Sagar just logged in
 
 
+                        // rest operator 
+// function calculateCartPrice(...num1){
+function calculateCartPrice(val1, val2, ...num1){    // Interview Question
+    return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
 
 
+// Objects in functions accesed while carting data 
+
+// TypeSafety must be check while developing 
+const user ={
+    username: "sagar",
+    prices: 199 
+}
+
+function handleObject(anyobject)
+{
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+handleObject({
+    username:"Sam",
+    price: 399
+});
+
+
+// Array Function for access cart data
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
 
 
